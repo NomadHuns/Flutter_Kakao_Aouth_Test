775446bc8beee58af3e699eb5add3244d58783ab
kimalss22 <kimalss22@gmail.com>
2023-05-06 15:30:59 +0900

    카카오 초기화 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_aouth_test/login_page.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_auth.dart';
 
 void main() {
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  KakaoSdk.init(
+    nativeAppKey: '0e844976288d2fd4a76d9f86d972e6aa',
+  );
+
   runApp(const MyApp());
 }
 
